ea197dd43b3cb057a871585cd1c99ee5ec4c3378
Sean Ryan <sean@designingsean.com>
2014-05-03 21:31:28 -0700

    Tweak where clause in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
   def index
     @users = Users.select(:id, :name, :active) \
-                      .where(:active => 1) \
+                      .where(active: true) \
                       .order(:name)
     render json: @users
   end
 
   def inactive
     @users = Users.select(:id, :name, :active) \
-                      .where(:active => 0) \
+                      .where(active: false) \
                       .order(:name)
     render json: @users
   end
